80776a4431da65b0c3f36dc846ee1bf5f38e9b74
Owen Yaggy <oyaggy@haverford.edu>
2025-01-31 23:18:40 -0500

    initial password

--- A02/password.c
+++ A02/password.c
@@ -1,9 +1,23 @@
 /***************************************************
- * Author: 
- * 
+ * Author: Owen Yaggy
+ * Implements a bad password algorithm
  */
 #include <stdio.h>
+#include <string.h>
 
 int main() {
+  char password[64]; // potential passwords are limited to 64 characters
+  printf("Enter a word: ");
+  scanf("%s", password);
+  for (int i = 0; i < strlen(password); i++) {
+    if (password[i] == 'e') {
+      password[i] = '3';
+    } else if (password[i] == 'l') {
+      password[i] = '1';
+    } else if (password[i] == 'a') {
+      password[i] = '@';
+    }
+  }
+  printf("Your bad password is %s\n", password);
   return 0;
 }
