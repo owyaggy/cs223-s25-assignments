075f2376188200f935a78cdc3c07c51183ffd573
owyaggy <owyaggy@icloud.com>
2025-02-16 20:31:25 -0500

    A05 initial

--- A05/read_ppm.c
+++ A05/read_ppm.c
@@ -1,7 +1,9 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Owen Yaggy
+ * Date: 2/16/25
+ * Description: Implements a program that reads
+ * PPM files and creates a flat 2D array of
+ * pixel structs.
  ---------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,7 +13,39 @@
 // Choose *one* to implement (do not remove the other one!)
 
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
-  return NULL;
+  FILE *infile;
+  infile = fopen(filename, "r"); // relative path name of file, read mode (textbook)
+  if (infile == NULL) {
+    printf("Error: unable to open file %s\n", filename);
+    return NULL; // specification requires returning NULL if file can't be loaded
+  }
+
+  struct ppm_pixel *pixels;
+  pixels = malloc(sizeof(struct ppm_pixel) * (*w) * (*h));
+  if (!pixels) {
+    printf("Error: malloc failed\n");
+    return NULL; // specification requires returning NULL if memory can't be allocated
+  }
+
+  // read header lines (inflexible, but one comment is valid assumption under specs)
+  char header[256];
+  fgets(header, 255, infile); // gets magic number
+  printf("%s", header);
+  fgets(header, 255, infile); // gets comment
+  printf("%s", header);
+  fgets(header, 255, infile); // gets dimensions
+  printf("%s", header);
+  fgets(header, 255, infile); // gets maxval
+  printf("%s", header);
+
+  for (int ch = fgetc(infile), i = 0; ch != EOF; ch = fgetc(infile), i++) {
+    printf("i: %d\tch: %d\n", i, ch);
+    // TODO: assignment is not working, fix!
+    if (i % 3 == 0) pixels[i / 3].red = ch;
+    else if (i % 3 == 1) pixels[i / 3].green = ch;
+    else if (i % 3 == 2) pixels[i / 3].blue = ch;
+  }
+  return pixels;
 }
 
 struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
--- A05/test_read.c
+++ A05/test_read.c
@@ -1,17 +1,30 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Owen Yaggy
+ * Date: 2/16/25
+ * Description: Calls a function to read the
+ * contents of a ppm file.
  ---------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "read_ppm.h"
 
 int main() {
   int w, h;
+  w = h = 4;
+  char filename[64];
+  strncpy(filename, "feep-raw.ppm", 64);
   struct ppm_pixel* pixels = read_ppm("feep-raw.ppm", &w, &h);
 
   // todo: print out the grid of pixels
+  printf("Testing file %s: %d %d\n", filename, w, h);
+  for (int i = 0; i < w; i++) {
+    for (int j = 0; j < h; j++) {
+      printf("(%3d,%3d,%3d) ", pixels[i*w + j].red, pixels[i*w + j].green, pixels[i*w + j].blue);
+    }
+    printf("\n");
+  }
+
   free(pixels);
   return 0;
 }
