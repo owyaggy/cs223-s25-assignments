6d3580553737a9f43788504edd933e56f1fbeb09
owyaggy <owyaggy@icloud.com>
2025-02-18 14:44:53 -0500

    progress on ascii

--- A05/ascii_image.c
+++ A05/ascii_image.c
@@ -1,12 +1,38 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Owen Yaggy
+ * Date: 2/18/25
+ * Description: Takes a PPM image as a command
+ * line argument and displays it as ASCII art.
  ---------------------------------------------*/
 #include <stdio.h>
 #include "read_ppm.h"
 #include "write_ppm.h"
 
-int main() {
+int main(int argc, char *argv[]) {
+  // get dimensions
+  FILE *infile;
+  infile = fopen(argv[1], "rb");
+  if (!infile) {
+    printf("Error - file not found\n");
+    exit(1);
+  }
+  int w, h;
+  int *dimensions;
+  dimensions = read_header(infile);
+  fclose(infile);
+  w = dimensions[0];
+  h = dimensions[1];
+  free(dimensions);
+
+  // get raster
+  struct ppm_pixel *pixels;
+  pixels = read_ppm(argv[1], w, h);
+  printf("Reading %s with width %d and height %d", argv[1], w, h);
+  for (int i = 0; i < w; i++) {
+    for (int j = 0; j < h; j++) {
+      // assign intensity for each pixel
+    }
+  }
+  free(pixels);
   return 0;
 }
--- A05/read_ppm.c
+++ A05/read_ppm.c
@@ -10,6 +10,27 @@
 #include <string.h>
 #include "read_ppm.h"
 
+// read the header lines of a PPM file
+// infile: an opened file stream
+// returns an array of integer dimensions (width then height)
+// NOTE: Caller is responsible for freeing the returned array
+int* read_header(FILE *infile) {
+  int *dimensions;
+  dimensions = malloc(sizeof(int) * 2);
+  // read header lines (inflexible, but one comment is valid assumption under specs)
+  char header[256];
+  fgets(header, 255, infile); // gets magic number
+  fgets(header, 255, infile); // gets comment
+  fgets(header, 255, infile); // gets dimensions
+  // split dimensions into width and height
+  char *dimension_chars;
+  dimension_chars = strtok(header, " ");
+  dimensions[0] = dimension_chars[0];
+  dimensions[1] = dimension_chars[1];
+  fgets(header, 255, infile); // gets maxval
+  return dimensions;
+}
+
 // Choose *one* to implement (do not remove the other one!)
 
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
@@ -27,12 +48,8 @@ struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
     return NULL; // specification requires returning NULL if memory can't be allocated
   }
 
-  // read header lines (inflexible, but one comment is valid assumption under specs)
-  char header[256];
-  fgets(header, 255, infile); // gets magic number
-  fgets(header, 255, infile); // gets comment
-  fgets(header, 255, infile); // gets dimensions
-  fgets(header, 255, infile); // gets maxval
+  // read header (function returns pointer to dimensions array)
+  free(read_header(infile));
 
   // get raster
   fread(pixels, sizeof(struct ppm_pixel), (*w)*(*h), infile);
