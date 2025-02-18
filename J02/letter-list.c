35030a6b1b29cabde778e649ac6469f18a165754
owyaggy <owyaggy@icloud.com>
2025-02-18 16:51:20 -0500

    J02 done

--- J02/letter-list.c
+++ J02/letter-list.c
@@ -1,4 +1,75 @@
+// Authors: Owen and Lina
+// Date: 2/18/25
+// Description: Implements a linked list of user-inputted
+// letters and removes all instances of a give letter
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct letter{
+  char l;
+  struct letter* next;
+};
+
+void insert_first(char c, struct letter** head){
+  struct letter* n = malloc(sizeof(struct letter));
+  n->l = c;
+  n->next = *head;
+  *head = n;
+}
+
+void printList(struct letter* head){
+  struct letter* n = head;
+  while (n != NULL){
+    printf("%c",n->l);
+    n = n->next;
+  }
+  printf("\n");
+}
+
+void clear(struct letter* head){
+  struct letter* current = head;
+  while (current != NULL) {
+    struct letter* next = current->next;
+    free(current);
+    current = next;
+  }
+}
+
+void removeL(struct letter* head, char r){
+  if (head->l == r) {
+    if (head->next) {
+      struct letter* next = head->next;
+      head->l = next->l;
+      head->next = next->next;
+      free(next);
+    }
+  }
+  if (head->next){
+    if (head->next->l == r) {
+      struct letter* temp;
+      temp = head->next;
+      head->next = head->next->next;
+      free(temp);
+    }
+    removeL(head->next,r);
+  }
+}
 int main()
 {
+  char str[32];
+  printf("Enter a word: ");
+  scanf("%s",str);
+  char s;
+  printf("Enter a character to remove: ");
+  scanf("%s", &s);
+  struct letter* head = NULL;
+  for (int i = strlen(str); i>=0 ; i--){
+    insert_first(str[i], &head);
+  }
+  removeL(head,s);
+  printList(head);
+  clear(head);
   return 0;
 }
--- J02/strblock.c
+++ J02/strblock.c
@@ -1,4 +1,41 @@
+// Authors: Owen and Lina
+// Date: 2/18/25
+// Description: Stores a given string in a 2D array
+// where the string is written across the top and
+// bottom rows, as well as the main diagonal connecting
+// the top-left and bottom-right corners
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 int main()
 {
+  char str[32];
+  printf("Enter a string:");
+  scanf("%s",str);
+  int len = strlen(str);
+  char* array = malloc(sizeof(char)*len*len);
+  for (int i = 0; i<len; i++){
+    if (i == 0 || i == len-1){
+      for (int j = 0; j < len; j++){
+        array[i*len +j] = str[j];
+        printf("%c ", str[j]);
+      }
+      printf("\n");
+    }
+    else{
+      for (int j = 0; j < len; j++){
+        if (i ==j){
+          array[i*len+j]= str[j];
+          printf("%c ", str[j]);
+        }
+        else{
+          printf("  ");
+        }
+      }
+      printf("\n");
+    }
+  }
+  free(array);
   return 0;
 }
