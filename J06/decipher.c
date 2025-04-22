b4992ae2558c5c14104b390fa3ec56bbab194e28
owyaggy <owyaggy@icloud.com>
2025-04-22 15:19:09 -0400

    q2

--- J06/decipher.c
+++ J06/decipher.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-    FILE* fp = fopen("message", "r");
+    char* filename = argv[1];
+    FILE* fp = fopen(filename, "r");
     char c = fgetc(fp);
     while (c != EOF)
     {
-        printf("%c", c);
+        printf("%c", (c >> 1) & 0b01111111);
         c = fgetc(fp);
     }
+    fclose(fp);
     return 0;
 }
