6fa049831179530d7b82bbe458f4bc529b06c788
owyaggy <owyaggy@icloud.com>
2025-04-22 15:24:21 -0400

    q3

--- J06/shot.c
+++ J06/shot.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+void sigint_handler(int sig) {
+  printf("Help! I've been shot!\n");
+  fflush(stdout);
+  exit(0);
+}
 
 int main()
 {
+  if (signal(SIGINT, sigint_handler) == SIG_ERR) {
+    printf("Error call to signal, SIGINT\n");
+    exit(1);
+  }
+
+  while(1) {
+    pause();
+  }
   return 0;
 }
